feaec963c43063244b0f828880aea59a4e12a71d
FrankTCA <frank@steamedveggie.com>
2022-09-03 17:50:46 -0400

    unlink() to rmdir()

--- php/action_update.php
+++ php/action_update.php
@@ -37,7 +37,7 @@ function copyfolder ($from, $to, $ext="{,.}*") {
         $ff = basename($a); // CURRENT FILE/FOLDER
         if (is_dir($a)) {
             copyfolder("$from$ff/", "$to$ff/");
-            unlink($a);
+            rmdir($a);
         } else {
             if (!copy($a, "$to$ff")) { exit("Error copying $a to $to$ff"); }
             echo "$a copied to $to$ff\n";
