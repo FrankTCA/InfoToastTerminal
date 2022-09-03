89b8982831c20d9402f6af22111689f1e7ed11b9
FrankTCA <frank@steamedveggie.com>
2022-09-03 17:42:11 -0400

    Add GLOB_BRACE

--- php/action_update.php
+++ php/action_update.php
@@ -28,7 +28,7 @@ function copyfolder ($from, $to, $ext="{.}*") {
     }
 
     // (A3) GET ALL FILES + FOLDERS IN SOURCE
-    $all = glob("$from$ext", GLOB_MARK);
+    $all = glob("$from$ext", GLOB_MARK | GLOB_BRACE);
     print_r($all);
 
     // (A4) COPY FILES + RECURSIVE INTERNAL FOLDERS
