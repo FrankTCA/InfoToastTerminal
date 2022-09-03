d43aaa396be2d0541c76d8d1ff5e2fc03b8c7a3c
FrankTCA <frank@steamedveggie.com>
2022-09-03 15:54:31 -0400

    Fix updater.sh timestamp incrementing automatically

--- runtime/updater.sh
+++ runtime/updater.sh
@@ -1,10 +1,13 @@
 #!/usr/local/bin/zsh
 # shellcheck disable=SC2225
-SECONDS=$(date +%s)
-cp -rv ~/terminal "~/term-backup/$SECONDS"
-echo "Saved as /datastore/html/term-backup/$SECONDS"
+THETIME=$(date +%s)
+BACKUPFILE="term-backup/$THETIME"
+cp -rv ~/terminal "$HOME/$BACKUPFILE"
+echo "Saved as /datastore/html/$BACKUPFILE"
 echo "Deleting terminal directory."
 rm -rf ~/terminal
 echo "Cloning via git."
 git clone --recursive https://github.com/FrankTCA/InfoToastTerminal ~/terminal
+echo "Moving credentials file."
+cp "$HOME/$BACKUPFILE/php/creds.php" "$HOME/terminal/php/creds.php"
 echo "[[;green;]Completed!]"
